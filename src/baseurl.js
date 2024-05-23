49d3b5a2deeb09447759372c9bed40d33937f895
ashutosh chaudhary <ashutosh.ch@damsdelhi.com>
2024-05-23 15:59:57 +0530

    error handles

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,2 +1,2 @@
-export const baseurl ="http://localhost:5000";
-// export const baseurl ="https://codematrixbackend.onrender.com";
+// export const baseurl ="http://localhost:5000";
+export const baseurl ="https://codematrixbackend.onrender.com";
